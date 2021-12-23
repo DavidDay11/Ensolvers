bbe7002a5aafe48a7c06d94489626096f7357d79
1W3110869DavidDay <69654369+DavidDay11@users.noreply.github.com>
2021-12-23 09:03:18 -0300

    Delete front directory

--- front/delete.js
+++
@@ -1,7 +0,0 @@
-function deleteTodo(ind) {
-    let todo = localStorage.getItem("todo");
-    todoArray = JSON.parse(todo);
-    todoArray.splice(ind, 1);
-    localStorage.setItem("todo", JSON.stringify(todoArray));
-    displayTodo();
-   }
--- front/function.js
+++
@@ -1,5 +0,0 @@
-const text = document.getElementById("text");
-const addTaskButton = document.getElementById("add-task-btn");
-const saveTaskButton = document.getElementById("save-todo-btn");
-const listBox = document.getElementById("listBox");
-const saveInd = document.getElementById("saveIndex");
